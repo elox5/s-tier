2b0e3d50a243ca223da42dbb3ba21c2610d3e1ef
elox5 <elox55555@gmail.com>
2024-03-10 14:47:46 +0100

    Add basic app structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,38 @@
 <script lang="ts">
 </script>
 
-<main></main>
+<main>
+  <div class="tier-container border"></div>
+  <div class="image-list border"></div>
+</main>
 
 <style>
+  main {
+    width: 1280px;
+    height: 800px;
+    display: grid;
+
+    grid-template-columns: 3fr 2fr;
+    grid-template-rows: 1fr;
+    gap: 10px;
+  }
+
+  .border {
+    border: 1px solid rgba(255, 255, 255, 0.4);
+    border-radius: 15px;
+  }
+
+  .tier-container {
+    grid-area: 1 / 1 / 2 / 2;
+
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
+  }
+
+  .image-list {
+    grid-area: 1 / 2 / 2 / 3;
+
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+  }
 </style>
