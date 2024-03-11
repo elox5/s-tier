afc120dc2eae2f8d14a53133c90dff6855173c71
elox5 <elox55555@gmail.com>
2024-03-11 22:11:10 +0100

    Fixing deployment part 3

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  build: {
-    outDir: "dist"
-  }
+  base: "/s-tier/",
 })
 
